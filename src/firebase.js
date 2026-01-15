fbc94c17cbb15da75e4d81c6afcf28ac7dbfe4e6
kayblezzy25 <kayblezzy25@gmail.com>
2026-01-14 18:34:17 -0600

    Homepage Mafe

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAEZ4Z5Z-AoiKIV49AMFUp8q0ls39MjId0",
-  authDomain: "sandboz-b6493.firebaseapp.com",
-  projectId: "sandboz-b6493",
-  storageBucket: "sandboz-b6493.firebasestorage.app",
-  messagingSenderId: "1003821462428",
-  appId: "1:1003821462428:web:b47bc0915d8aece800ce59",
-  measurementId: "G-JY9JY45PMY"
+  apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
